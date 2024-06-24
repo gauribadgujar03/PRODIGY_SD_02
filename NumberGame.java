ff2173662ed148fd13e470b1e91ff4fac762ca75
gauribadgujar03 <gauribadgujar03@gmail.com>
2024-06-24 18:39:47 +0530

    Initial commit

---
+++ NumberGame.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1; // Change this to set the lower bound of the range
+        int upperBound = 100; // Change this to set the upper bound of the range
+        int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+        
+        int attempts = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between " + lowerBound + " and " + upperBound + ". Try to guess it!");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < randomNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > randomNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You've guessed the number " + randomNumber + " correctly in " + attempts + " attempts!");
+            }
+        }
+
+        scanner.close();
+    }
+}
